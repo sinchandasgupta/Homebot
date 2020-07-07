68e035ce0175eda4dcf0c92a8b514bb7714efe67
Sinucifer09 <sinchandsgpt9@gmail.com>
2020-07-07 08:02:37 +0000

    Posp-ify: added Potato Project support

--- modules/ci/posp.sh
+++ modules/ci/posp.sh
@@ -16,18 +16,18 @@
 #
 
 # This value will also be used for folder name
-CI_AOSP_PROJECT=ProjectFluid-10.0
+CI_AOSP_PROJECT=posp
 # Name to display on Telegram post
-CI_AOSP_PROJECT_NAME="Project Fluid"
+CI_AOSP_PROJECT_NAME="Potato Project Q"
 # Android version to display on Telegram post
 CI_AOSP_PROJECT_VERSION=10
 # These next 2 values are needed to lunch (e.g. "lineage"_whyred-"userdebug")
-CI_LUNCH_PREFIX=fluid
+CI_LUNCH_PREFIX=potato
 CI_LUNCH_SUFFIX=userdebug
 # Target to build (e.g. to build a ROM's OTA package, use "bacon" or "otapackage", for a recovery project, use "recoveryimage")
-CI_BUILD_TARGET=bacon
+CI_BUILD_TARGET=
 # Filename of the output. You can also use wildcards if the name isn't fixed
-CI_OUT_ARTIFACTS_NAME=FluidOS-*.zip
+CI_OUT_ARTIFACTS_NAME=potato_vince-10-*.zip
 
 # Don't touch this line, unless you know what you are doing
 modules/ci/AOSP.sh "$@" --project "$CI_AOSP_PROJECT" --name "$CI_AOSP_PROJECT_NAME" --version "$CI_AOSP_PROJECT_VERSION" --lunch_prefix "$CI_LUNCH_PREFIX" --lunch_suffix "$CI_LUNCH_SUFFIX" --build_target "$CI_BUILD_TARGET" --artifacts "$CI_OUT_ARTIFACTS_NAME"
